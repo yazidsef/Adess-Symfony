db860d54925362a8d30b2b3557fc47798146b214
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-09-29 11:11:30 +0200

    adding the crud of the events

---
+++ src/Form/EventsType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Events;
+use App\Entity\Room;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('date_start', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('end_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('paying')
+            ->add('prix')
+            ->add('numberofregistrants')
+            ->add('salle', EntityType::class, [
+                'class' => Room::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Events::class,
+        ]);
+    }
+}
